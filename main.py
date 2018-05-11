884b3a9f64557cb05b28b11f05efeef076e6281e
Jason Poindexter <jason@launchbadge.com>
2018-05-10 18:36:34 -0700

    Add calibration status parsing. Reduce loop sleep. Support for data logging

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ sensor = bno055.BNO055(i2c, address=41)
 #sensor.operation_mode(bno055.COMPASS_MODE)
 sensor.operation_mode(bno055.NDOF_MODE)
 #sensor.operation_mode(bno055.M4G_MODE)
-
+#with open('bno055_readings.txt', 'a+') as file:
 while True:
     try:
         ex, ey, ez = sensor.euler()
@@ -37,8 +37,11 @@ while True:
         #yawdegrees = yaw * 180/math.pi
         #print("% 4.2f % 4.2f % 4.2f" % (ex, yaw, yawdegrees))
         calib_stat = sensor.calib_stat()
+        st_result = sensor.st_result()
+        sys_error = sensor.sys_error()
+        sys_status = sensor.sys_status()
 
-        print("% 4.2f % 4.2f %s result=%d err=%d status=%d" % (ex, yaw, bin(calib_stat), sensor.st_result(), sensor.sys_error(), sensor.sys_status()))
+        print("% 4.2f % 4.2f %s result=%d err=%d status=%d" % (ex, yaw, bin(calib_stat), st_result, sys_error, sys_status))
 
 
         mag_status = calib_stat & 3
@@ -49,16 +52,27 @@ while True:
         print("mag_status=%d acc_status=%d gyr_status=%d sys_status=%d" % (mag_status, acc_status, gyr_status, sys_status))
 
 
-
         acc = "x=%d y=%d z=%d" % sensor.acc_offset()
         mag = "x=%d y=%d z=%d" % sensor.mag_offset()
         gyr = "x=%d y=%d z=%d" % sensor.gyr_offset()
+
         sensor.operation_mode(bno055.CONFIG_MODE)
+
         print("cal acc=(%s) mag=(%s) gyr=(%s) acc_r=%d mag_r=%d" % (acc, mag, gyr, sensor.acc_radius(), sensor.mag_radius()))
+
+
+        #file.write("%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (str(ex), str(yaw), str(st_result), str(sys_error), str(sys_status), str(mag_status), str(acc_status), str(gyr_status), str(sys_status)))
+        # file.write(str(ex))
+
         sensor._system_trigger(1) # Run a self test.
         sensor.operation_mode(bno055.NDOF_MODE)
         #print("% 4.2f % 4.2f % 4.2f  % -50s %d %s %s" % (x, y, z, sensor.quaternion(), sensor.temperature(), bin(sensor.calib_stat()[0]), bin(sensor.calib_stat()[1])))
+
     except OSError as e:
         print(dir(e))
-    time.sleep(1)
+
+    time.sleep(.1)
+
+
+
 
